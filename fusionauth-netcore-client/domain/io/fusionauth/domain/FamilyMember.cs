d104e9d9612a81e24b50585129061d7ec260b258
Daniel DeGroff <daniel@inversoft.com>
2019-06-14 11:50:46 -0600

    update domain from 1.7.0 release

---
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/FamilyMember.cs
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+
+using System.Collections.Generic;
+using System;
+
+namespace io.fusionauth.domain {
+
+  /**
+   * Models a single family member.
+   *
+   * @author Brian Pontarelli
+   */
+  public class FamilyMember {
+    public Dictionary<string, object> data;
+    public DateTimeOffset? insertInstant;
+    public bool? owner;
+    public FamilyRole role;
+    public Guid? userId;
+
+    public FamilyMember with(Action<FamilyMember> action) {
+      action(this);
+      return this;
+    }
+  }
+}
