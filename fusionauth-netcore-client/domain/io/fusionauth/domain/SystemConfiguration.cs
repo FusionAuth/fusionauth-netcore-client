f9d673b44ad183f5bdcb62b3adf35dabe5559ccb
Daniel DeGroff <daniel@fusionauth.io>
2020-09-05 23:13:15 -0600

    Remove SystemConfiguration.cookieEncryptionKey from client libs.

--- fusionauth-netcore-client/domain/io/fusionauth/domain/SystemConfiguration.cs
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/SystemConfiguration.cs
@@ -27,8 +27,6 @@ namespace io.fusionauth.domain {
 
     public AuditLogConfiguration auditLogConfiguration;
 
-    public string cookieEncryptionKey;
-
     public CORSConfiguration corsConfiguration;
 
     public Dictionary<string, object> data;
