f68ff5f1c650f86dadd33fcd50f091a771a0ff62
Daniel DeGroff <daniel@fusionauth.io>
2021-04-16 03:11:29 -0600

    Domain sync

--- fusionauth-netcore-client/domain/io/fusionauth/domain/ApplicationFormConfiguration.cs
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/ApplicationFormConfiguration.cs
@@ -27,7 +27,7 @@ namespace io.fusionauth.domain {
 
     public Guid? adminRegistrationFormId;
 
-    public Guid? selfServiceUserFormId;
+    public Guid? selfServiceFormId;
 
     public ApplicationFormConfiguration with(Action<ApplicationFormConfiguration> action) {
       action(this);
