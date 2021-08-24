65f868de74516c7c4c29e34bfd8706ee9a90ee74
Ryan Dennis <38541110+RyanDennis2018@users.noreply.github.com>
2021-08-24 14:57:18 -0400

    Update DateTimeOffsetConverter.cs
    removed two spaces per tab indent

--- fusionauth-netcore-client/src/io/fusionauth/converters/DateTimeOffsetConverter.cs
+++ fusionauth-netcore-client/src/io/fusionauth/converters/DateTimeOffsetConverter.cs
@@ -3,38 +3,38 @@ using Newtonsoft.Json;
 
 namespace io.fusionauth.converters
 {
-    public class DateTimeOffsetConverter : JsonConverter
+  public class DateTimeOffsetConverter : JsonConverter
+  {
+    public override bool CanRead => true;
+
+    public override bool CanWrite => true;
+
+    public override bool CanConvert(Type objectType)
     {
-
-        public override bool CanRead => true;
-
-        public override bool CanWrite => true;
-
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(DateTimeOffset) || objectType == typeof(DateTimeOffset?);
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null)
-            {
-                return null;
-            }
-
-            var utc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds((long)reader.Value);
-            return new DateTimeOffset(utc);
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            if (value == null)
-            {
-                return;
-            }
-
-            var epoch = ((DateTimeOffset)value - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
-            writer.WriteValue(epoch);
-        }
+      return objectType == typeof(DateTimeOffset) || objectType == typeof(DateTimeOffset?);
     }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+      if (reader.TokenType == JsonToken.Null)
+      {
+        return null;
+      }
+
+      var value = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds((long)reader.Value);
+      return new DateTimeOffset(value);
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+      if (value == null)
+      {
+        return;
+      }
+
+      var millis = ((DateTimeOffset)value - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+      writer.WriteValue(millis);
+    }
+  }
 }
+
