1bf39164db82a201f05fda5bbcddb3f062833824
Daniel DeGroff <daniel@fusionauth.io>
2022-10-12 11:23:39 -0600

    Domain sync

--- fusionauth-netcore-client/src/io/fusionauth/FusionAuthSyncClient.cs
+++ fusionauth-netcore-client/src/io/fusionauth/FusionAuthSyncClient.cs
@@ -24,6 +24,7 @@ using io.fusionauth.domain.api.jwt;
 using io.fusionauth.domain.api.passwordless;
 using io.fusionauth.domain.api.report;
 using io.fusionauth.domain.api.twoFactor;
+using io.fusionauth.domain.api.webauthn;
 using io.fusionauth.domain.api.user;
 using io.fusionauth.domain.oauth2;
 using io.fusionauth.domain.provider;
