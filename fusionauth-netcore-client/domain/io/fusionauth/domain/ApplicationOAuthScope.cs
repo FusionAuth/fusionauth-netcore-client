8dacc7958a2d465082e7c07fe0a7fcdefd54e2c9
Spencer Witt <spencer@fusionauth.io>
2024-03-27 15:59:26 -0500

    new domain objects for EAP release

---
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/ApplicationOAuthScope.cs
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018-2023, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+
+using System.Collections.Generic;
+using System;
+
+namespace io.fusionauth.domain
+{
+
+  /**
+   * A custom OAuth scope for a specific application.
+   *
+   * @author Spencer Witt
+   */
+  public class ApplicationOAuthScope {
+
+    public Guid? applicationId;
+
+    public IDictionary<string, object> data;
+
+    public string defaultConsentDetail;
+
+    public string defaultConsentMessage;
+
+    public string description;
+
+    public Guid? id;
+
+    public DateTimeOffset? insertInstant;
+
+    public DateTimeOffset? lastUpdateInstant;
+
+    public string name;
+
+    public bool? required;
+
+    public ApplicationOAuthScope with(Action<ApplicationOAuthScope> action) {
+      action(this);
+      return this;
+    }
+  }
+}
---
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/oauth2/ProvidedScopePolicy.cs
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018-2023, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+
+using io.fusionauth.domain;
+using System.Collections.Generic;
+using System;
+
+namespace io.fusionauth.domain.oauth2
+{
+
+  /**
+   * The policy for provided scope handling
+   *
+   * @author Spencer Witt
+   */
+  public class ProvidedScopePolicy {
+
+    public Requirable address;
+
+    public Requirable email;
+
+    public Requirable phone;
+
+    public Requirable profile;
+
+    public ProvidedScopePolicy with(Action<ProvidedScopePolicy> action) {
+      action(this);
+      return this;
+    }
+  }
+}
