907805b6444b23fc68e0b6269256d4fc1804ae03
Brent Halsey <brent.halsey@fusionauth.io>
2025-04-17 11:20:57 -0600

    remove application requirePassword, add tenant passwordEnabled

--- fusionauth-netcore-client/domain/io/fusionauth/domain/RegistrationConfiguration.cs
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/RegistrationConfiguration.cs
@@ -43,8 +43,6 @@ namespace io.fusionauth.domain
 
     public Requirable preferredLanguages;
 
-    public bool? requirePassword;
-
     public RegistrationType type;
 
     public RegistrationConfiguration with(Action<RegistrationConfiguration> action) {
