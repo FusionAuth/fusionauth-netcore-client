59c59af4bb7fa08960e3838be85b3175379aa7a3
Brady Wied <brady.wied@fusionauth.io>
2025-06-12 11:21:02 -0600

    client update

--- fusionauth-netcore-client/domain/io/fusionauth/domain/TenantRateLimitConfiguration.cs
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/TenantRateLimitConfiguration.cs
@@ -34,8 +34,6 @@ namespace io.fusionauth.domain
 
     public RateLimitedRequestConfiguration sendPasswordless;
 
-    public RateLimitedRequestConfiguration sendPasswordlessPhone;
-
     public RateLimitedRequestConfiguration sendPhoneVerification;
 
     public RateLimitedRequestConfiguration sendRegistrationVerification;
