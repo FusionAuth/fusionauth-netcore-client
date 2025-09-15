f37cd6d8b0570ea958df12d920a29f843839bdee
Brady Wied <brady.wied@fusionauth.io>
2025-09-15 15:20:12 -0600

    fix comment capitalization

--- fusionauth-netcore-client/src/io/fusionauth/IFusionAuthClient.cs
+++ fusionauth-netcore-client/src/io/fusionauth/IFusionAuthClient.cs
@@ -234,7 +234,7 @@ namespace io.fusionauth {
     /// This is an asynchronous method.
     /// </summary>
     /// <param name="loginId"> The loginId of the User that you intend to change the password for.</param>
-    /// <param name="loginIdTypes"> the identity types that FusionAuth will compare the loginId to.</param>
+    /// <param name="loginIdTypes"> The identity types that FusionAuth will compare the loginId to.</param>
     /// <returns>
     /// When successful, the response will contain the log of the action. If there was a validation error or any
     /// other type of error, this will return the Errors object in the response. Additionally, if FusionAuth could not be
@@ -3528,7 +3528,7 @@ namespace io.fusionauth {
     /// This is an asynchronous method.
     /// </summary>
     /// <param name="loginId"> The email or username of the user.</param>
-    /// <param name="loginIdTypes"> the identity types that FusionAuth will compare the loginId to.</param>
+    /// <param name="loginIdTypes"> The identity types that FusionAuth will compare the loginId to.</param>
     /// <returns>
     /// When successful, the response will contain the log of the action. If there was a validation error or any
     /// other type of error, this will return the Errors object in the response. Additionally, if FusionAuth could not be
@@ -3722,7 +3722,7 @@ namespace io.fusionauth {
     /// <param name="loginId"> The userId id.</param>
     /// <param name="start"> The start instant as UTC milliseconds since Epoch.</param>
     /// <param name="end"> The end instant as UTC milliseconds since Epoch.</param>
-    /// <param name="loginIdTypes"> the identity types that FusionAuth will compare the loginId to.</param>
+    /// <param name="loginIdTypes"> The identity types that FusionAuth will compare the loginId to.</param>
     /// <returns>
     /// When successful, the response will contain the log of the action. If there was a validation error or any
     /// other type of error, this will return the Errors object in the response. Additionally, if FusionAuth could not be
@@ -5298,7 +5298,7 @@ namespace io.fusionauth {
    /// An HTTP status code of 400 with a general error code of [TrustTokenRequired] indicates that a Trust Token is required to make a POST request to this API.
    /// </summary>
    /// <param name="loginId"> The loginId of the User that you intend to change the password for.</param>
-   /// <param name="loginIdTypes"> the identity types that FusionAuth will compare the loginId to.</param>
+   /// <param name="loginIdTypes"> The identity types that FusionAuth will compare the loginId to.</param>
    /// <returns>
    /// When successful, the response will contain the log of the action. If there was a validation error or any
    /// other type of error, this will return the Errors object in the response. Additionally, if FusionAuth could not be
@@ -8355,7 +8355,7 @@ namespace io.fusionauth {
    /// Retrieves the user for the loginId, using specific loginIdTypes.
    /// </summary>
    /// <param name="loginId"> The email or username of the user.</param>
-   /// <param name="loginIdTypes"> the identity types that FusionAuth will compare the loginId to.</param>
+   /// <param name="loginIdTypes"> The identity types that FusionAuth will compare the loginId to.</param>
    /// <returns>
    /// When successful, the response will contain the log of the action. If there was a validation error or any
    /// other type of error, this will return the Errors object in the response. Additionally, if FusionAuth could not be
@@ -8536,7 +8536,7 @@ namespace io.fusionauth {
    /// <param name="loginId"> The userId id.</param>
    /// <param name="start"> The start instant as UTC milliseconds since Epoch.</param>
    /// <param name="end"> The end instant as UTC milliseconds since Epoch.</param>
-   /// <param name="loginIdTypes"> the identity types that FusionAuth will compare the loginId to.</param>
+   /// <param name="loginIdTypes"> The identity types that FusionAuth will compare the loginId to.</param>
    /// <returns>
    /// When successful, the response will contain the log of the action. If there was a validation error or any
    /// other type of error, this will return the Errors object in the response. Additionally, if FusionAuth could not be
