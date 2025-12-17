dce029c05fe18db80570ad80dfbef23f0eae8f4d
Lyle Schemmerling <lshem16@gmail.com>
2025-12-17 13:24:31 -0700

    commit new domain files

---
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/oauth2/OAuthCodeAccessTokenRequest.cs
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018-2025, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+
+using System.Collections.Generic;
+using System;
+
+namespace io.fusionauth.domain.oauth2
+{
+
+  /**
+   * The request object for exchanging an OAuth authorization code for an access token.
+   *
+   * @author Lyle Schemmerling
+   */
+  public class OAuthCodeAccessTokenRequest {
+
+    public string client_id;
+
+    public string client_secret;
+
+    public string code;
+
+    public string grant_type;
+
+    public string redirect_uri;
+
+    public string tenantId;
+
+    public OAuthCodeAccessTokenRequest with(Action<OAuthCodeAccessTokenRequest> action) {
+      action(this);
+      return this;
+    }
+  }
+}
