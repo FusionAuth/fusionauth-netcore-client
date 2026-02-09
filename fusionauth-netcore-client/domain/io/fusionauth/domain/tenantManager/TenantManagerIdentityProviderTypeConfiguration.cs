b361c1ead1f6d57db029aea16d53f815130c912a
Spencer Witt <3409780+spwitt@users.noreply.github.com>
2026-02-09 09:46:20 -0600

    add TenantManagerIdentityProviderTypeConfiguration domain object ENG-3779

---
+++ fusionauth-netcore-client/domain/io/fusionauth/domain/tenantManager/TenantManagerIdentityProviderTypeConfiguration.cs
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018-2026, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+
+using io.fusionauth.domain;
+using io.fusionauth.domain.provider;
+using System.Collections.Generic;
+using System;
+
+namespace io.fusionauth.domain.tenantManager
+{
+
+  /**
+   * Configuration object for identity provider types allowed in Tenant Manager
+   */
+  public class TenantManagerIdentityProviderTypeConfiguration: Enableable {
+
+    public IDictionary<string, string> defaultAttributeMappings;
+
+    public DateTimeOffset? insertInstant;
+
+    public DateTimeOffset? lastUpdateInstant;
+
+    public IdentityProviderLinkingStrategy linkingStrategy;
+
+    public IdentityProviderType type;
+
+    public TenantManagerIdentityProviderTypeConfiguration with(Action<TenantManagerIdentityProviderTypeConfiguration> action) {
+      action(this);
+      return this;
+    }
+  }
+}
